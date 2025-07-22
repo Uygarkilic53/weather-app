8309588d7e0f5fa2a0549c67a34d2809b01908ee
Uygar <79859143+Uygarkilic53@users.noreply.github.com>
2025-07-22 16:35:55 +0300

    A Weather App built with React.JS
    Features Instant Weather Lookup: Get current weather conditions for any city worldwide. User-Friendly Interface: Simple input field for city name and clear display of weather information. Real-time Data: Fetches up-to-date weather details directly from the OpenWeatherMap API. Single-Page Experience: All information displayed on the same page for convenience.
    
    Technologies Used ReactJS: Frontend library for building user interfaces. HTML5: Structure of the web pages. CSS3: Styling and layout. JavaScript (ES6+): Application logic. OpenWeatherMap API: For fetching weather data.
    
    Installation To get a local copy up and running, follow these simple steps.
    
    Prerequisites Make sure you have Node.js and npm (Node Package Manager) or yarn installed on your machine.
    
    Node.js & npm: Download Node.js (npm is included with Node.js installation).
    
    Steps 1-Clone the repository:
    
        git clone https://github.com/uygarkilic53/ReactWeatherApp.git
        cd ReactWeatherApp
    
    2- Install dependencies:
    
    Using npm:
    npm install
    Configure API Key:
    
    This application requires an API key from OpenWeatherMap to fetch weather data.
    
    Go to OpenWeatherMap and sign up for a free account.
    
    Once signed in, navigate to the "API keys" tab in your profile to find or generate your API key.
    
    Add your API key to the line 5 in src folder
    
    Usage After completing the installation and API key configuration steps:
    
    1-Start the development server:
    
    Using npm:
    
        npm run start
    
    2- Open your browser:
    
    The application will automatically open in your default web browser at http://localhost:3000.
    
    3- Enter a City:
    
    Type the name of a city into the input field and press Enter or click the "Search" button. The weather information for that city will be displayed on the page.
    License Distributed under the MIT License. See LICENSE for more information.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const apiKey = "YOUR API KEY";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+          console.log(data);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Enter City..."
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      /> 
+    
+      {typeof weatherData.main === "undefined" ? (
+        <div className="welcome">
+          <p>Welcome to weather app! Enter in a city to get the weather of.</p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className="temp">{Math.round(weatherData.main.temp)}</p>
+          <p className="weather">{weatherData.weather && weatherData.weather[0]?.main}</p>
+        </div>
+      )}
+
+      {weatherData.cod=== "404" ? (
+        <p>City Not Found</p>
+      ): (
+        <>
+        
+        </>
+      )}
     </div>
   );
 }
